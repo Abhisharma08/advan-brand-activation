c0944c3003b29b013b904dafe6224f4085c3060e
Gautam Adhikari <144870605+gautamadhikari19@users.noreply.github.com>
2026-05-14 15:22:37 +0530

    basepath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  basePath: '',
+  basePath: '/BrandActivation',
   skipTrailingSlashRedirect: true,
   /* config options here */
   typescript: {
